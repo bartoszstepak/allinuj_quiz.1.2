70507cfc79ea8985e647834c9ab68db0e5fc8f4f
bartoszstepak <46901072+bartoszstepak@users.noreply.github.com>
2021-03-16 11:22:48 +0100

    Update kultura.jsx

--- src/components/results/kultura.jsx
+++ src/components/results/kultura.jsx
@@ -73,7 +73,7 @@ export default class kultura extends React.Component {
                     Jesteś człowiekiem kultury! W piątki chodzisz do kina, a wino Ci najbardziej smakuje na wernisażach, to dwa z wielu powodów, dla których filar Kultura powinien na stałe zagościć w Twoim życiu. Masz zapał do poznawania krakowskich artystów, a muzyka jest lekiem na każde Twoje zmartwienie. Urokliwe spacery i odkrywanie Krakowskich zakamarków nie jest Ci obce. Moda? Jasne! Cenisz sobie elegancję i styl, jednak nie zamykasz się w jednym trendzie.                    
                     </span><br /><br/><br/>
                     <button className="back_btn" style={buttonColors} onClick={() => this.props.history.push('/')}>Zagraj ponownie</button>
-                    <a rel="noopener noreferrer" href="https://www.facebook.com/events/360442518612991" target="_blank">
+                    <a rel="noopener noreferrer" href="https://www.facebook.com/events/238086861383992" target="_blank">
                         <button className="next_btn">Zobacz rekrutację</button>
                     </a>
                 </div>
