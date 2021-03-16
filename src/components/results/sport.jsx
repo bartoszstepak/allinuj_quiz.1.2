fa7a7fa14ebde61b5a7c6f8d85992b4b519d5cd9
bartoszstepak <46901072+bartoszstepak@users.noreply.github.com>
2021-03-16 11:23:10 +0100

    Update sport.jsx

--- src/components/results/sport.jsx
+++ src/components/results/sport.jsx
@@ -74,7 +74,7 @@ export default class Sport extends React.Component {
                         Jesteś  typem sportowego świra, oczywiście w samych pozytywach tego słowa znaczeniu! Lubisz wyzwania i z determinacją osiągasz kolejne postawione sobie cele. Dobrze wiesz, że w zdrowym ciele, zdrowy duch. Ważne jest dla Ciebie prowadzenie zdrowego i aktywnego stylu życia! Bieganie, siłownia czy sporty zespołowe nie są Ci obce. Czujesz się w tym jak ryba w wodzie. Jednak sportowiec to nie tylko ciągłe ćwiczenia, ale też czas na odpoczynek czy zabawę.  Z pewnością wtedy lubisz również oglądnąć dobry film, czy sięgnąć po książkę czy spędzić miło czas w towarzystwie! Oby tak dalej!
                     </span><br/><br/><br/>
                     <button className="back_btn" style={buttonColors} onClick={() => this.props.history.push('/')}>Zagraj ponownie</button>
-                    <a rel="noopener noreferrer" href="https://www.facebook.com/events/360442518612991" target="_blank">
+                    <a rel="noopener noreferrer" href="https://www.facebook.com/events/238086861383992" target="_blank">
                         <button className="next_btn">Zobacz rekrutację</button>
                     </a>
                 </div>
